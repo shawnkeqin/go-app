880de943e768cbc7703484244e2b335045a21deb
Shawn <e0052482@u.nus.edu>
2021-10-10 20:33:33 +0800

    first commit

---
+++ src/Main.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+
+var (
+ actorName string = "Elizabeth"
+ companion string = "Sarah"
+ doctorName int = 3
+ season int = 11
+)
+
+var (counter int = 0)
+
+func main(){
+//	i := 42
+//	var j float32 = 27
+	fmt.Printf("%v, %T", companion,companion)
+}
+
