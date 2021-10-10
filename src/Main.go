2b6810a6b42f24b456b627b1c6a6d626c3dc27ba
Shawn <e0052482@u.nus.edu>
2021-10-10 22:02:38 +0800

    primitives

--- src/Main.go
+++ src/Main.go
@@ -1,25 +1,65 @@
 package main 
 
 import ("fmt"
-"strconv")
-
-
-var (
- actorName string = "Elizabeth"
- companion string = "Sarah"
- doctorName int = 3
- season int = 11
 )
 
-var (counter int = 0)
+
+// var (
+//  actorName string = "Elizabeth"
+//  companion string = "Sarah"
+//  doctorName int = 3
+//  season int = 11
+// )
+
+// var (counter int = 0)
 
 func main(){
-	var i int = 42
-	fmt.Printf("%v, %T\n",i,i )
+	var r rune= 'a'
+	fmt.Printf("%v, %T\n", r, r)
+	var n complex128 = complex(5,12)
+	fmt.Printf("%v, %T\n", n,n)
 
-	var j string 
-	j = strconv.Itoa(i)
-	fmt.Printf("%v, %T\n",j,j)
+	s := "this is a string"
+	fmt.Printf("%v, %T\n", string(s[2]), s[2])
+	// a := 10.2
+	// b := 3.7
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+	// n := 42
+	// var n uint16 = 42
+	// fmt.Printf("%v, %T\n",n,n)
+	// a := 10
+	// b := 3
+	// fmt.Println(a+b)
+	// fmt.Println(a-b)
+	// fmt.Println(a*b)
+	// fmt.Println(a+b)
+	// fmt.Println(a%b)
+	// var a int = 10
+	// var b int8 = 3
+	// fmt.Println(a + int(b))
+	// a := 10
+	// b := 3
+	// fmt.Println(a & b)
+	// fmt.Println( a | b)
+	// fmt.Println(a ^  b)
+	// fmt.Println(a &^ b)
+	// a := 8
+	// fmt.Println(a << 3)
+	// fmt.Println(a >> 3)
+	// var n bool = true 
+
+	// n := 1 == 1
+	// m := 1 == 2
+	// fmt.Printf("%v, %T\n",n,n)
+// 	var i int = 42
+// 	fmt.Printf("%v, %T\n",i,i )
+
+// 	var j string 
+// 	j = strconv.Itoa(i)
+// 	fmt.Printf("%v, %T\n",j,j)
 	// var theHTTP string = "https://google.com"
 //	i := 42
 //	var j float32 = 27
