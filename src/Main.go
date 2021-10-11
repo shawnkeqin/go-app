73feeae3dfa4c7096621561b5a52a5bfc441c416
Shawn <e0052482@u.nus.edu>
2021-10-11 19:30:19 +0800

    constants

--- src/Main.go
+++ src/Main.go
@@ -13,14 +13,34 @@ import ("fmt"
 
 // var (counter int = 0)
 
-func main(){
-	var r rune= 'a'
-	fmt.Printf("%v, %T\n", r, r)
-	var n complex128 = complex(5,12)
-	fmt.Printf("%v, %T\n", n,n)
+const a int16 = 27 
 
-	s := "this is a string"
-	fmt.Printf("%v, %T\n", string(s[2]), s[2])
+const (
+	x = iota 
+	y = iota
+	z = iota 
+)
+func main(){
+	const a int = 42
+	const b string = "foo"
+	const c float32 = 3.14
+	const d bool = true
+	const e int = 39
+	fmt.Printf("%v, %T\n", a + e, a + e)
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%v\n", d)
+	fmt.Printf("%v\n, %T\n", x, x)
+	fmt.Printf("%v\n, %T\n", y, y)
+	fmt.Printf("%v\n, %T\n", z, z)
+	// var r rune= 'a'
+	// fmt.Printf("%v, %T\n", r, r)
+	// var n complex128 = complex(5,12)
+	// fmt.Printf("%v, %T\n", n,n)
+
+	// s := "this is a string"
+	// fmt.Printf("%v, %T\n", string(s[2]), s[2])
 	// a := 10.2
 	// b := 3.7
 	// fmt.Println(a + b)
