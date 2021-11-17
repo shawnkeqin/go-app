cbe28c8f70cc0ba36d7cd3d55c93c9cabaaf21be
Shawn <e0052482@u.nus.edu>
2021-11-18 01:03:03 +0800

    defer

--- src/Main.go
+++ src/Main.go
@@ -67,17 +67,18 @@ func returnTrue() bool {
 }
 
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello Go!"))
-	})
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err.Error())
-	}
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	// 	w.Write([]byte("Hello Go!"))
+	// })
+	// err := http.ListenAndServe(":8080", nil)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
 
 
 
 	fmt.Println("start")
+	defer fmt.Println("this was deferred")
 	panic("something bad happened")
 	fmt.Println("end")
 
